e9312ea8e3d011963b533003e72b3df4b4786e23
Sonterix <sonterixo@gmail.com>
2017-11-26 17:55:10 +0200

    added header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 (function(){
 
+    // click nav 
+    $('nav a').on('click', function(){
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+    })
+    
+    // animate scroll
+    $('body').on('click', 'a[href^="#"]', function() {
+        var clicked = $(this).attr("href"),
+        destination = $(clicked).offset().top;            
+
+        $("html,body").animate({
+            scrollTop: destination
+        }, 1200);
+
+        return false;
+    });
 })(jQuery);
